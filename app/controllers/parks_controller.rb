0437dc4780215f105388383ef8247cb97f159266
knoddin <knoddin@gmail.com>
2016-11-19 23:57:48 -0500

    removed unnecessary methods on parks controller.

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,5 +1,5 @@
 class ParksController < ApplicationController
-  before_action :set_park, only: [:show, :update, :destroy]
+  before_action :set_park, only: [:show]
 
   # GET /parks
   # GET /parks.json
@@ -13,38 +13,6 @@ class ParksController < ApplicationController
   # GET /parks/1.json
   def show
     render json: @park
-  end
-
-  # POST /parks
-  # POST /parks.json
-  def create
-    @park = Park.new(park_params)
-
-    if @park.save
-      render json: @park, status: :created, location: @park
-    else
-      render json: @park.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /parks/1
-  # PATCH/PUT /parks/1.json
-  def update
-    @park = Park.find(params[:id])
-
-    if @park.update(park_params)
-      head :no_content
-    else
-      render json: @park.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /parks/1
-  # DELETE /parks/1.json
-  def destroy
-    @park.destroy
-
-    head :no_content
   end
 
   private
